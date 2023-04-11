7d0e26019341eeeb42e5fecead0e598fac6026a0
Yuvraj <122032242+Yuvraj-source@users.noreply.github.com>
2023-04-11 11:53:46 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -3,18 +3,14 @@ import ListItems from './ListItems';
 
 const List = ({ listx }) => {
   return (
-    <>
-      <ul id="list">
-      
-        <>
-          <li className="items" style={{ display: 'none' }}>Placeholder</li>
-          {listx.map((item) => (
-            <ListItems key={item} value={item} />
-          ))}
-        </>
-      </ul>
-    </>
+    <ul id="list">
+      <li className="items" style={{ display: 'none' }}>Placeholder</li>
+      {listx.map((item) => (
+        <ListItems key={item} value={item} />
+      ))}
+    </ul>
   );
 }
 
 export default List;
+
